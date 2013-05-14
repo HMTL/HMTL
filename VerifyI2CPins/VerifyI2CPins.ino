0f15621f9ace1fe6fb8e3c3d010bb342b6f47ed8
Adam Phelps <amp@cs.stanford.edu>
2013-05-14 01:42:58 -0700

    Added simple test code to very the arduino's I2C pins are working

---
+++ VerifyI2CPins/VerifyI2CPins.ino
@@ -0,0 +1,32 @@
+/*
+  Blink
+  Turns on an LED on for one second, then off for one second, repeatedly.
+ 
+  This example code is in the public domain.
+ */
+ 
+// Pin 13 has an LED connected on most Arduino boards.
+// give it a name:
+int led = 13;
+
+// the setup routine runs once when you press reset:
+void setup() {            
+  Serial.begin(9600);
+  
+  // initialize the digital pin as an output.
+  pinMode(led, OUTPUT);     
+  pinMode(A4, INPUT);
+  pinMode(A5, INPUT);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  Serial.print(analogRead(A4));
+  Serial.print("-");
+  Serial.println(analogRead(A5));
+  
+  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(500);               // wait for a second
+  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+  delay(500);               // wait for a second
+}
