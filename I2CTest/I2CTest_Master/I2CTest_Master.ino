3cdbc745c6fd2893f1c3d2e7e08c09b7efa641f8
Adam Phelps <amp@cs.stanford.edu>
2013-05-18 20:48:33 -0700

    Allow the master to update any pin on the slaves

--- I2CTest/I2CTest_Master/I2CTest_Master.ino
+++ I2CTest/I2CTest_Master/I2CTest_Master.ino
@@ -26,14 +26,30 @@ void setup()
 }
 
 #define NUM_SLAVES 6
-boolean led_on[NUM_SLAVES] = {false, false, false, false, false, false};
+#define NUM_PINS 4
+boolean led_on[NUM_SLAVES][NUM_PINS] = {
+  {false, false},
+  {false, false},
+  {false, false},
+  {false, false},
+  {false, false},
+  {false, false}
+};
 int slave_id[NUM_SLAVES] = {0, 3, 2, 4, 1, 5};
+int slave_pin[NUM_SLAVES][NUM_PINS] = {
+  {9, 10, 11, 12},
+  {9, 10, 11, 12},
+  {9, 10, 11, 12},
+  {9, 10, 11, 12},
+  {9, 10, 11, 12},
+  {9, 10, 11, 12},
+};
 
 boolean debug_led = false;
 
 int cycle = 0;
 
-#define MODE   2
+#define MODE   3
 #define PERIOD 250
 
 void loop()
@@ -56,25 +72,46 @@ void loop()
   cycle++;
 }
 
+/* Update hte state of all devices */
 void update_state() 
 {
   int slave;
-  for (slave = 0; slave < NUM_SLAVES; slave++) {
 
-    switch (MODE) {
-        case 0:
-          led_on[slave] = !led_on[slave];
-          break;
-        case 1:
-          led_on[slave] = (slave == (cycle % NUM_SLAVES));
-          break;
-        case 2:
-          led_on[slave] = (slave == (cycle % NUM_SLAVES));
-          break;
-    }
+
+  switch (MODE) {
+      case 0:
+        for (slave = 0; slave < NUM_SLAVES; slave++) {
+          led_on[slave][0] = !led_on[slave][0];
+        }
+        break;
+      case 1:
+        for (slave = 0; slave < NUM_SLAVES; slave++) {
+          led_on[slave][0] = (slave == (cycle % NUM_SLAVES));
+        }
+        break;
+      case 2:
+        for (slave = 0; slave < NUM_SLAVES; slave++) {
+          led_on[slave][0] = (slave == (cycle % NUM_SLAVES));
+          led_on[slave][1] = (slave == (cycle % NUM_SLAVES));
+        }
+        break;
+      case 3:
+        static int current_pin = 0;
+        static int current_slave = 0;
+        static boolean current_on = true;
+
+        led_on[current_slave][current_pin] = current_on;
+
+        if (!current_on) {
+          current_pin = (current_pin + 1) % NUM_PINS;
+          if (current_pin == 0) current_slave = (current_slave + 1) % NUM_SLAVES;
+        }
+        current_on = !current_on;
+        break;
   }
 }
 
+/* Send a state update to the indicated device */
 void send_state(int slave) 
 {
   Wire.beginTransmission(slave_id[slave]);
@@ -82,18 +119,24 @@ void send_state(int slave)
   switch (MODE) {
       case 0:
       case 1:
-        Wire.write(PIN_STATUS_LED);               // Pin to set
-        if (led_on[slave]) Wire.write(PACKET_ON); // Pin value
+        Wire.write(slave_pin[slave][1]);               // Pin to set
+        if (led_on[slave][0]) Wire.write(PACKET_ON); // Pin value
         else Wire.write(PACKET_OFF);
         break;
       case 2:
-        Wire.write(12);               // Pin to set
-        if (led_on[slave]) Wire.write(PACKET_ON); // Pin value
+        Wire.write(slave_pin[slave][0]);               // Pin to set
+        if (led_on[slave][0]) Wire.write(PACKET_ON); // Pin value
         else Wire.write(PACKET_OFF);
-        Wire.write(11);               // Pin to set
-        if (led_on[slave]) Wire.write(PACKET_ON); // Pin value
+        Wire.write(slave_pin[slave][1]);               // Pin to set
+        if (led_on[slave][1]) Wire.write(PACKET_ON); // Pin value
         else Wire.write(PACKET_OFF);
-
+        break;
+      case 3:
+        for (int pin = 0; pin < NUM_PINS; pin++) {
+          Wire.write(slave_pin[slave][pin]);               // Pin to set
+          if (led_on[slave][pin]) Wire.write(PACKET_ON); // Pin value
+          else Wire.write(PACKET_OFF);
+        }
         break;
   }
 
