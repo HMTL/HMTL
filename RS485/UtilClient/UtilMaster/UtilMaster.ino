f58f04d6e3172919fc2ab1e7971db15f733268f5
Adam Phelps <amp@cs.stanford.edu>
2013-06-26 22:21:28 -0700

    Initial pin addressing

--- RS485/UtilClient/UtilMaster/UtilMaster.ino
+++ RS485/UtilClient/UtilMaster/UtilMaster.ino
@@ -1,42 +1,151 @@
 
 #include <RS485_non_blocking.h>
 #include <SoftwareSerial.h>
+#include <EEPROM.h> // XXX
+#include <Wire.h> // XXX
 
+#include "I2CUtils.h" // XXX
 #include "RS485Utils.h"
 
 RS485Socket rs485(2, 3, 4, true);
 
-#define LED_PIN 13
+#define PIN_STATUS_LED 12
+#define PIN_DEBUG_LED  13
 
-byte databuffer[256];
-byte *data;
+#define NUM_SLAVES 6
+#define NUM_PINS 3
+boolean led_value[NUM_SLAVES][NUM_PINS] = {
+  {0, 0, 0},
+  {0, 0, 0},
+  {0, 0, 0},
+  {0, 0, 0},
+  {0, 0, 0},
+  {0, 0, 0}
+};
+int slave_id[NUM_SLAVES] = {0, 1, 2, 3, 4, 5};
+int slave_pin[NUM_SLAVES][NUM_PINS] = {
+  {9, 10, 11},
+  {9, 10, 11},
+  {9, 10, 11},
+  {9, 10, 11},
+  {9, 10, 11},
+  {9, 10, 11},
+};
+
+#define MAX_MSG_DATA     (NUM_PINS * sizeof(message_t))
+#define SEND_BUFFER_SIZE (MAX_MSG_DATA + sizeof (rs485_socket_hdr_t) + 64)
+
+byte databuffer[SEND_BUFFER_SIZE];
+byte *send_buffer;
 
 void setup()
 {
   Serial.begin(9600);
-  pinMode (LED_PIN, OUTPUT);  // driver output enable
+
+  pinMode(PIN_STATUS_LED, OUTPUT);
+  digitalWrite(PIN_STATUS_LED, HIGH);
+
+  pinMode(PIN_DEBUG_LED, OUTPUT);
+  digitalWrite(PIN_DEBUG_LED, HIGH);
 
   rs485.setup();
-
-  data = rs485.initBuffer(databuffer);
+  send_buffer = rs485.initBuffer(databuffer);
 }
 
-byte i = 0;
-boolean b = false;
-byte address = 0;
 
-#define NUM_DEVICES 2
+boolean debug_led = false;
 
-void loop() {
-  i++;
+int cycle = 0;
 
-  *(int *)data = i;
+#define MODE   3
+#define PERIOD 100
 
-  rs485.sendMsgTo(address, data, sizeof i);
+void loop()
+{
+  long start = millis();
+  int slave;
 
-  b = !b;
-  digitalWrite(LED_PIN, b);
+  update_state();
 
-  address = (address + 1) % NUM_DEVICES;
-  delay(500);
+  for (slave = 0; slave < NUM_SLAVES; slave++) {
+    send_state(slave);
+  }
+
+  debug_led = !debug_led;
+  if (debug_led) digitalWrite(PIN_DEBUG_LED, HIGH);
+  else digitalWrite(PIN_DEBUG_LED, LOW);
+
+  int elapsed = millis() - start;
+  delay(PERIOD - (elapsed > 0 ? elapsed : 0));
+  cycle++;
+}
+
+/* Update hte state of all devices */
+void update_state() 
+{
+  int slave;
+  int pin;
+
+  switch (MODE) {
+      case 0:
+        for (slave = 0; slave < NUM_SLAVES; slave++) {
+          led_value[slave][0] = (led_value[slave][0] == 0 ? 255 : 0);
+        }
+        break;
+      case 1:
+        for (slave = 0; slave < NUM_SLAVES; slave++) {
+          led_value[slave][0] = ((slave == (cycle % NUM_SLAVES)) ? 255 : 0);
+        }
+        break;
+      case 2:
+        for (slave = 0; slave < NUM_SLAVES; slave++) {
+          led_value[slave][0] = ((slave == (cycle % NUM_SLAVES)) ? 255 : 0);
+          led_value[slave][1] = ((slave == (cycle % NUM_SLAVES)) ? 255 : 0);
+        }
+        break;
+      case 3:
+        static int current_pin = 0;
+        static int current_slave = 0;
+        static boolean current_on = true;
+
+        led_value[current_slave][current_pin] = (current_on ? 255 : 0);
+
+        if (!current_on) {
+          current_pin = (current_pin + 1) % NUM_PINS;
+          if (current_pin == 0) current_slave = (current_slave + 1) % NUM_SLAVES;
+        }
+        current_on = !current_on;
+        break;
+      case 4:
+        for (slave = 0; slave < NUM_SLAVES; slave++) {
+          for (pin = 0; pin < NUM_PINS; pin++) {
+            led_value[slave][pin] = (led_value[slave][pin] + 10) % 256;
+          }
+        }
+
+        break;
+  }
+}
+
+/* Send a state update to the indicated device */
+void send_state(int slave) 
+{
+  /* Fill in the send buffer with this address's pin state */
+  message_t *msg;
+  unsigned int msg_len = 0;
+  for (int pin = 0; pin < NUM_PINS; pin++) {
+    if (msg_len > (MAX_MSG_DATA - sizeof (message_t))) {
+      /* Prevent buffer overflow */
+      Serial.println("ERROR: send_state: msg exceeded length");
+      break;
+    }
+
+    msg = (message_t *)(&send_buffer[0] + pin * sizeof (message_t));
+    msg_len += sizeof (message_t);
+
+    msg->pin = slave_pin[slave][pin];
+    msg->value = led_value[slave][pin];
+  }
+
+  rs485.sendMsgTo(slave_id[slave], send_buffer, msg_len);
 }
--- RS485/UtilClient/UtilSlave/UtilSlave.ino
+++ RS485/UtilClient/UtilSlave/UtilSlave.ino
@@ -1,30 +1,102 @@
 
 #include <RS485_non_blocking.h>
 #include <SoftwareSerial.h>
+#include <EEPROM.h> // XXX
+#include <Wire.h> // XXX
 
+
+#include "I2CUtils.h" // XXX
 #include "RS485Utils.h"
 
 RS485Socket rs485(2, 3, 4, true);
 
-#define LED_PIN 13
+#define PIN_DEBUG_LED 13
+
+int my_address = 1;
+
+#define NUM_PINS 3
+byte pin_num[NUM_PINS] = {
+  10, 11, 12
+};
+byte pin_value[NUM_PINS] = {
+  0, 0, 0,
+};
+
 
 void setup()
 {
   Serial.begin(9600);
-  pinMode (LED_PIN, OUTPUT);  // driver output enable
+
+  for (int pin_index = 0; pin_index < NUM_PINS; pin_index++) {
+    int pin = pin_num[pin_index];
+    pinMode(pin, OUTPUT);
+    digitalWrite(pin, LOW);
+  }
+
+  pinMode(PIN_DEBUG_LED, OUTPUT);  // driver output enable
 
   rs485.setup();
 }
 
-byte i = 0;
+
 boolean b = false;
-byte address = 0;
+
+boolean update;
 
 void loop() {
-  const byte *data = rs485.getMsg(address);
-  if (data != NULL) {
-    i = *(data);
-    b = !b;
-    digitalWrite(LED_PIN, b);
+  update = false;
+
+  /* Check for messages to this address */
+  read_state();
+
+  /* If state changed then updated all pins */
+  if (update) {
+    for (int pin_index = 0; pin_index < NUM_PINS; pin_index++) {
+      int pin = pin_num[pin_index];
+      if (pin_is_PWM(pin)) {
+        if (pin_value[pin] == 0) digitalWrite(pin, LOW);
+        else if (pin_value[pin] == 255) digitalWrite(pin, HIGH);
+        else analogWrite(pin, pin_value[pin]);
+      } else {
+        if (pin_value[pin] > 0) digitalWrite(pin, HIGH);
+        else digitalWrite(pin, LOW);
+      }
+    }
+  }
+
+  blink_value(PIN_DEBUG_LED, my_address, 500, 4);
+
+  delay(100);
+}
+
+void read_state() 
+{
+  unsigned int msglen;
+
+  const byte *data = rs485.getMsg(my_address, &msglen);
+  if ((data != NULL) && (msglen >= sizeof (message_t))) {
+    message_t *msg = (message_t *)data;
+    unsigned int processed = 0;
+
+    Serial.print("read_state:");
+    do {
+      if (processed > (msglen - sizeof (message_t))) {
+        Serial.print("ERROR: read_state: msg_len wasn't multiple of message_t");
+        break;
+      }
+
+      Serial.print(" ");
+      Serial.print(msg->pin);
+      Serial.print(":");
+      Serial.print(msg->value);
+
+      pin_value[msg->pin] = msg->value;
+
+      processed += sizeof (message_t);
+      msg = msg + 1;
+    } while (processed < msglen);
+    Serial.println();
+
+    update = true;
   }
 }
