4bc67a98a841254e70f1c1c4a5d6a764981c8f8f
Adam Phelps <amp@opendns.com>
2014-05-08 00:22:37 -0700

    Minor updates

--- HMTL_Modules/HMTLPythonConfig/TailArduino.py
+++ HMTL_Modules/HMTLPythonConfig/TailArduino.py
@@ -13,21 +13,18 @@ def handle_args():
     parser = OptionParser()
     parser.add_option("-d", "--device", dest="device",
                       help="Arduino USB device")
-    parser.add_option("-c", "--chooseport", dest="chooseport", action="store_true",
-                      help="Choose port from list of available ports", default=False)
 
     parser.add_option("-v", "--verbose", dest="verbose", action="store_true",
                       help="Verbose output", default=False)
 
     (options, args) = parser.parse_args()
 
-    # Required args
-    if (options.chooseport):
+    if (options.device == None):
         options.device = portscan.choose_port()
 
     if (options.device == None):
-        parser.print_help()
-        exit("Must specify device");
+	    parser.print_help()
+            exit("Must specify device");
 
     return (options, args)
 
