c6119a700daaaa86bea5cfacf153f090af2e3c1d
Adam Phelps <amp@opendns.com>
2014-05-26 11:20:34 -0400

    Separated HMTL Client and Server

--- HMTL_Modules/HMTLPython/HMTLClient.py
+++ HMTL_Modules/HMTLPython/HMTLClient.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python
 #
-# Launch a HMTL command server
+# Launch a client/server HMTL command system
 
 import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__), "lib"))
 
 from optparse import OptionParser
-from server import *
 from client import *
-import portscan
 
 def handle_args():
     global options
 
     parser = OptionParser()
 
-    parser.add_option("-d", "--device", dest="device",
-                      help="Arduino USB device")
-    parser.add_option("-n", "--dryrun", dest="dryrun", action="store_true",
-                      help="Perform dryrun only", default=False)
+    parser.add_option("-r", "--rgb", dest="setrgb", action="store_true",
+                      help="Set RGB value", default=False)
+    parser.add_option("-P", "--period", dest="period", type="float",
+                      help="Sleep period between changes")
+    parser.add_option("-A", "--hmtladdress", dest="hmtladdress", type="int",
+                      help="Address to which messages are sent")
 
     parser.add_option("-v", "--verbose", dest="verbose", action="store_true",
                       help="Verbose output", default=False)
@@ -27,27 +27,20 @@ def handle_args():
     parser.add_option("-a", "--address", dest="address",
                       help="Address to bind to", default="localhost")
 
-
     (options, args) = parser.parse_args()
     print("options:" + str(options) + " args:" + str(args))
 
-
-
-    if ((options.device == None) and (not options.dryrun)):
-        options.device = portscan.choose_port()
-    
     return (options, args)
  
 def main():
 
     handle_args()
 
-    server = HMTLServer(options)
-    server.listen()
-    server.close()
+    client = HMTLClient(options)
+    client.test()
+    client.close()
 
     print("Done.")
     exit(0)
 
 main()
-
