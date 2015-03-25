6d23f70bc746840b9ba881f9a8fc4c82d347e2ac
Adam Phelps <amp@opendns.com>
2015-03-24 19:12:03 -0700

    Myo script tweaking

--- python/myo_client.py
+++ python/myo_client.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python
 #
-#export PYTHONPATH=$PYTHONPATH:/Users/amp/Dropbox/Myo/myo-python/
-#export DYLD_LIBRARY_PATH="$DYLD_LIBRARY_PATH:/Users/amp/Dropbox/Myo/MyoSDK/myo.framework"
+# In order to run, the Myo SDK and the python-myo library must have their paths
+# added:
+#   export PYTHONPATH=$PYTHONPATH:/Users/amp/Dropbox/Myo/myo-python/
+#   export DYLD_LIBRARY_PATH="$DYLD_LIBRARY_PATH:/Users/amp/Dropbox/Myo/MyoSDK/myo.framework"
 
 import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__), "lib"))
 from optparse import OptionParser, OptionGroup
-
+from time import time
 
 import myo
 from myo.lowlevel import pose_t, stream_emg
@@ -19,12 +21,25 @@ import HMTLprotocol
 
 myo.init()
 
+class prettyfloat(float):
+    def __repr__(self):
+        return "%6.2f" % self
+
+def pf(data):
+    return map(prettyfloat, data)
+
 class Listener(myo.DeviceListener):
     # return False from any method to stop the Hub
+    OUTPUT_PERIOD = 0.25
 
     def __init__(self, client, options):
         self.client = client
         self.options = options
+        self.last_output = 0
+        self.data = {}
+        self.data["gyroscope"] = {"data": []}
+        self.data["acceleration"] = {"data": []}
+        self.data["orientation"] = {"data": []}
 
     def on_connect(self, myo, timestamp):
         print_("Connected to Myo")
@@ -54,34 +69,58 @@ class Listener(myo.DeviceListener):
     def on_pose(self, myo, timestamp, pose):
         print_('on_pose', pose)
 
-        msg = None
-        if pose == pose_t.double_tap:
-            msg = HMTLprotocol.get_rgb_msg(self.options.hmtladdress,
-                                           1,
-                                           255,0,255)
-        elif pose == pose_t.rest:
-            msg = HMTLprotocol.get_value_msg(self.options.hmtladdress,
-                                             1,
-                                             0)
-        elif pose == pose_t.wave_in:
-            msg = HMTLprotocol.get_rgb_msg(self.options.hmtladdress,
-                                           1,
-                                           0,255,0)
-        elif pose == pose_t.wave_out:
-            msg = HMTLprotocol.get_rgb_msg(self.options.hmtladdress,
-                                           1,
-                                           0,0,255)
-        elif pose == pose_t.fist:
-            msg = HMTLprotocol.get_rgb_msg(self.options.hmtladdress,
-                                           1,
-                                           255,255,255)
-        elif pose == pose_t.fingers_spread:
-            msg = HMTLprotocol.get_rgb_msg(self.options.hmtladdress,
-                                           1,
-                                           255,0,0)
+        self.data["on_pose"] = { "timestamp" : timestamp, "data" : pose }
 
-        if (msg != None):
-            self.client.send_and_ack(msg, False)
+        msgs = []
+        if pose == pose_t.double_tap:
+            msgs += [ HMTLprotocol.get_rgb_msg(self.options.hmtladdress,
+                                              1,
+                                              255,0,255) ]
+        elif pose == pose_t.rest:
+            msgs += [
+                HMTLprotocol.get_rgb_msg(self.options.hmtladdress,
+                                         1,
+                                         0,0,0),
+                
+                HMTLprotocol.get_value_msg(69,
+                                           0,
+                                           0),
+
+                HMTLprotocol.get_value_msg(69,
+                                           1,
+                                           0)
+            ]
+
+        elif pose == pose_t.wave_in:
+            msgs += [ HMTLprotocol.get_rgb_msg(self.options.hmtladdress,
+                                               1,
+                                               0,255,0) ]
+        elif pose == pose_t.wave_out:
+            msgs += [ HMTLprotocol.get_rgb_msg(self.options.hmtladdress,
+                                               1,
+                                               0,0,255) ]
+        elif pose == pose_t.fist:
+            msgs += [
+                HMTLprotocol.get_rgb_msg(self.options.hmtladdress,
+                                         1,
+                                         255,0,0),
+                HMTLprotocol.get_value_msg(69,
+                                           1,
+                                           255) 
+            ]
+        elif pose == pose_t.fingers_spread:
+            msgs += [ 
+                HMTLprotocol.get_rgb_msg(self.options.hmtladdress,
+                                         1,
+                                         255,255,255),
+                HMTLprotocol.get_value_msg(69,
+                                           0,
+                                           128) 
+                ]
+
+        if (len(msgs) != 0):
+            for msg in msgs:
+                self.client.send_and_ack(msg, False)
             print("Sent and acked to client")
 
     def on_unlock(self, myo, timestamp):
@@ -95,6 +134,27 @@ class Listener(myo.DeviceListener):
 
     def on_unsync(self, myo, timestamp):
         print_('unsynced')
+
+    def on_orientation_data(self, myo, timestamp, orientation):
+        self.data["orientation"] = { "timestamp" : timestamp, "data" : orientation }
+        self.update_output()
+
+    def on_accelerometor_data(self, myo, timestamp, acceleration):
+        self.data["acceleration"] = { "timestamp" : timestamp, "data" : acceleration }
+        self.update_output()
+
+    def on_gyroscope_data(self, myo, timestamp, gyroscope):
+        self.data["gyroscope"] = { "timestamp" : timestamp, "data" : gyroscope }
+        self.update_output()
+
+    def update_output(self):
+        if (time.time() - self.last_output > self.OUTPUT_PERIOD):
+            print("O%s A%s G%s" % (
+                pf(self.data["orientation"]["data"]),
+                pf(self.data["acceleration"]["data"]),
+                pf(self.data["gyroscope"]["data"]),
+            ))
+            self.last_output = time.time()
 
 def handle_args():
     global options
@@ -117,6 +177,8 @@ def handle_args():
 
 def main():
     options = handle_args()
+
+    print("%s\nOpening client.  Address=%d\n%s" % ("*"*80, options.hmtladdress, "*"*80))
 
     client = HMTLClient(options)
 
